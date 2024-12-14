55721b74a002d88180ef4c001fc2d04729f4247b
dheeraj07 <naluboludheerajreddy007@gmail.com>
2024-12-13 21:30:27 -0500

    reverting url changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  baseURL: process.env.BACKEND_ENDPOINT || "http://localhost:8000",
 });
 
 api.interceptors.request.use((config) => {
