cc6b2b4f6cafeecc88be8f21615d2d6d343a7603
dheeraj07 <naluboludheerajreddy007@gmail.com>
2024-12-14 00:17:09 -0500

    modified layout and hooks

--- frontend/src/components/layout/UserMenu.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const UserMenu: () => import("react/jsx-runtime").JSX.Element;
--- frontend/src/hooks/useMealPlan.d.ts
+++
@@ -1,7 +0,0 @@
-import { GenerateMealPlanRequest } from '../types/mealplan.types';
-export declare const useMealPlan: () => {
-    isLoading: boolean;
-    error: string | null;
-    generateMealPlan: (request: GenerateMealPlanRequest) => Promise<any>;
-    getMealPlanHistory: () => Promise<any>;
-};
